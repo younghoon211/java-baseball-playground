78d017f65a00fdbb8e68a501ce12e6019536fefc
younghoon211@naver.com <younghoon211@naver.com>
2022-11-05 20:40:41 +0900

    feat: split 메소드 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,24 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        String[] actual2 = "1".split(",");
+        assertThat(actual).contains("1", "2");
+        assertThat(actual2).containsExactly("1");
     }
 }
