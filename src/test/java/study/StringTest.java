2128ef553698e276dd989d8a6e9d6c0ba7d2bb18
younghoon211@naver.com <younghoon211@naver.com>
2022-11-05 20:41:23 +0900

    feat: substring 메소드 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -21,4 +21,10 @@ public class StringTest {
         assertThat(actual).contains("1", "2");
         assertThat(actual2).containsExactly("1");
     }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
 }
