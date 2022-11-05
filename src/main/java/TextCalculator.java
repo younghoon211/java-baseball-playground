d1c65a947d88a81addf1c9c05ae736dfc452011f
younghoon211@naver.com <younghoon211@naver.com>
2022-11-06 02:43:06 +0900

    feat: 사칙 연산 세부 메소드

--- src/main/java/TextCalculator.java
+++ src/main/java/TextCalculator.java
@@ -30,4 +30,24 @@ public class TextCalculator {
 
         return 0;
     }
+
+    public int plus(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("0으로는 나눌 수 없습니다.");
+        }
+
+        return a / b;
+    }
 }
